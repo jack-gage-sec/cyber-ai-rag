091d79644cf167726c877c1fb9dfe4617971e802
jack-gage-sec <jackthetechguy10137@gmail.com>
2026-08-05 16:45:08 -0400

    Delete synthetic_data directory

--- synthetic_data/test_generator.py
+++
@@ -1,9 +0,0 @@
-from users import generate_users
-from export import export_csv
-
-users = generate_users(500)
-
-export_csv(
-    users,
-    "users.csv"
-)
--- synthetic_data/users.py
+++
@@ -1,7 +0,0 @@
-from synthetic_data.shared_entities import EMPLOYEES
-
-import pandas as pd
-
-def generate_users():
-
-    return pd.DataFrame(EMPLOYEES)
